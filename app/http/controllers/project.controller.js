3f9b092f0e6bcdb53413918a7ae0e509f7799d79
mohammadmhosseini <moh79hosseini@gmail.com>
2022-06-20 20:17:12 +0430

    craete

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -2,14 +2,15 @@ const { ProjectModel } = require("../../models/project");
 
 class ProjectController {
   async createProject(req, res ,next) {
-    const {title, text, image} = req.body;
+    const {title, text, image, tags} = req.body;
     const { _id } = req.user;
 
     const result = await ProjectModel.create({
       title,
       text,
       owner : _id,
-      image
+      image,
+      tags
     });
     if(!result) throw { status : 400, message: "افزودن پروژه با مشکل مواجه شد"};
     return res.status(201).json({
--- app/http/validations/project.js
+++ app/http/validations/project.js
@@ -4,6 +4,7 @@ function createProjectValidator(){
     return [
         body("title").notEmpty().withMessage("لطفا عنوان پروژه را وارد کنید"),
         body("text").notEmpty().isLength({min : 20}).withMessage("لطفا توضیحات پروژه را وارد کنید و توضیحات باید حداقل 20 کاراکتر باشد"),
+        body("tags").isArray({min : 0, max : 10}).withMessage("حداکثر از 10 هشتگ میتوانید استفاده کنید")
     ];
 }
 
